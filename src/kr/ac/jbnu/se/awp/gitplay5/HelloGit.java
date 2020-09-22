8edb897e23b12123e6834a825b5bea4d3c15bafd
mycom <mycom@DESKTOP-BKNOVI1>
2020-09-22 17:01:33 +0900

    test

--- src/kr/ac/jbnu/se/awp/gitplay5/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay5/HelloGit.java
@@ -7,9 +7,12 @@ public class HelloGit {
 		System.out.println("Hello heejoo!!");
 		system.out.println("Hello im seongju!!!");
 		
+		system out println("conflict test");
+		
 		for (int i=0;i<5;i++)
 			system.out.println(i);
 		system.out.println("This is a confliction test.");
+	
 	}
 	//add commit : this seongju -> im modify
 }
